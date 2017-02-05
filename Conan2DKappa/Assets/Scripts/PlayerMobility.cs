b380b4f03604ab3126eef296fef391a66d48a0eb
Timo <cobravspanda01@gmail.com>
2017-02-05 19:57:05 +0100

    moving + animated character

--- Conan2DKappa/Assets/Scripts/PlayerMobility.cs
+++ Conan2DKappa/Assets/Scripts/PlayerMobility.cs
@@ -5,19 +5,67 @@ using UnityEngine;
 public class PlayerMobility : MonoBehaviour {
 
     public float speed;
-    public Rigidbody2D rb;
+
+    Animator animator;
+    // Use this for initialization
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-
-    // Use this for initialization
+    //Physics updates
     void FixedUpdate () {
         var move =new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += move * speed * Time.deltaTime;
     }
-	
+    //Not physics updates
+    void Update()
+    {
+        animationStopCheck();
+        //up
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetTrigger("moveUp");
+        }
+        //down
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetTrigger("moveDown");
+        }
+        //left
+        else if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetTrigger("moveLeft");
+        }
+        //right
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetTrigger("moveRight");
+        }
+    }
+    void animationStopCheck()
+    {
+        //up
+        if (!Input.GetKey(KeyCode.W))
+        {
+            animator.SetTrigger("stopUp");
+        }
+        //down
+        if (!Input.GetKey(KeyCode.S))
+        {
+            animator.SetTrigger("stopDown");
+        }
+        //left
+        if (!Input.GetKey(KeyCode.A))
+        {
+            animator.SetTrigger("stopLeft");
+        }
+        //right
+        if (!Input.GetKey(KeyCode.D))
+        {
+            animator.SetTrigger("stopRight");
+        }
+    }
 	
     
 }
