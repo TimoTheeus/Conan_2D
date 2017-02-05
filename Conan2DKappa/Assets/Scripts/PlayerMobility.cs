57bf07c16c12ace9c84b2cb62b711691f92e19e9
Timo <cobravspanda01@gmail.com>
2017-02-05 18:24:22 +0100

    project
    yes

---
+++ Conan2DKappa/Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Use this for initialization
+    void FixedUpdate () {
+        var move =new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+	
+	
+    
+}
